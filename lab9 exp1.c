e03ce40bd80b79823737bf746e0a7fce9be4dbdf
HARSH <126059255+teamharsh@users.noreply.github.com>
2023-03-11 17:27:48 +0530

    Lab 9

---
+++ lab9 exp1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int factorial(int n);
+int main() 
+{ 
+int number; 
+int fact; 
+printf("Enter a number: "); 
+scanf("%d", &number); 
+fact = factorial(number); 
+printf("Factorial of %d is %d\n", number, fact); 
+return 0; 
+}
+
+int factorial(int n)
+{ 
+if (n == 0) 
+return 1; 
+else
+return(n * factorial(n-1)); 
+} 
